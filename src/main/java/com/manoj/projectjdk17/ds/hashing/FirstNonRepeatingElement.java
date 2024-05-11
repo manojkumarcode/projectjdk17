3a6825451cd49509c6ccfbc6e117d3fa6364c293
Manoj Kumar <manoj.er.kumar@gmail.com>
2024-05-11 23:18:10 +0530

    Find the first Non-repeating element in an array int[] arr = {10,5,3,4,10,5,6}

---
+++ src/main/java/com/manoj/projectjdk17/ds/hashing/FirstNonRepeatingElement.java
@@ -0,0 +1,20 @@
+package com.manoj.projectjdk17.ds.hashing;
+
+import io.cucumber.java.hu.Ha;
+
+import java.util.HashSet;
+
+public class FirstNonRepeatingElement {
+
+    public static void main(String[] args) {
+        int[] arr = {10,5,3,4,10,5,6};
+        int firstNonRepeatingElement = Integer.MIN_VALUE;
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = arr.length-1; i >=0 ; i--) {
+            if(hashSet.add(arr[i])){
+                firstNonRepeatingElement = arr[i];
+            }
+        }
+        System.out.println("firstNonRepeatingElement: " + firstNonRepeatingElement);
+    }
+}
