1769af8b95fb21d51f7143774657c32249102d70
Manoj Kumar <manoj.er.kumar@gmail.com>
2024-05-12 20:29:52 +0530

    Reformat the code

--- src/main/java/com/manoj/projectjdk17/ds/Arrays/SmallestElementInArray.java
+++ src/main/java/com/manoj/projectjdk17/ds/Arrays/SmallestElementInArray.java
@@ -1,22 +1,19 @@
 package com.manoj.projectjdk17.ds.Arrays;
 
-import io.cucumber.java.sl.In;
-
 public class SmallestElementInArray {
 
     public static void main(String[] args) {
-        int[] arr = {2,6,9,10,-11,0,5};
+        int[] arr = {2, 6, 9, 10, -11, 0, 5};
         findSmallestInArray(arr);
     }
 
     private static void findSmallestInArray(int[] arr) {
         int smallest = arr[0];
-        for(int num : arr){
-            if(num < smallest){
+        for (int num : arr) {
+            if (num < smallest) {
                 smallest = num;
             }
         }
-
         System.out.println("Smallest Element : " + smallest);
     }
 }
