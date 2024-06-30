a19fbe31f46fc9fa95ba67ce38cd26f1103206ec
Manoj Kumar <manoj.er.kumar@gmail.com>
2024-06-30 19:47:17 +0530

    Graphs: define the graph using hashMap and implemented the BreadthFirstSearch algorithm using queue and visited array

--- src/main/java/com/manoj/projectjdk17/ds/graphs/BFSTraversal.java
+++ src/main/java/com/manoj/projectjdk17/ds/graphs/BFSTraversal.java
@@ -1,7 +1,9 @@
 package com.manoj.projectjdk17.ds.graphs;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BFSTraversal {
 
@@ -17,11 +19,35 @@ public class BFSTraversal {
     public void breadthFirstSearch(HashMap<Integer, List<Integer>> graph, int source, int noOfNodes){
 
         int[] visited = new int[noOfNodes];
+        printVisitedArray(visited);
+
+        Queue queue = new LinkedList();
+        queue.add(source);
+        visited[source] = 1;
+        System.out.print(source + ",");
+        while (!queue.isEmpty()){
+            int current = (int) queue.poll();
+            List<Integer> adjacents = graph.get(current);
+            for(int node: adjacents){
+                if(visited[node] == 0) {
+                    queue.add(node);
+                    visited[node] = 1;
+                    System.out.print(node + ",");
+                }
+            }  // All the adjacent nodes visited
+
+        }
+
+
+    }
+
+    private static void printVisitedArray(int[] visited) {
         System.out.print("Visited array:  ");
         for (int i = 0; i < visited.length ; i++) {
             System.out.print(i + ":" + visited[i] + ", ");
         }
         System.out.println();
-
     }
+
+
 }
--- src/main/java/com/manoj/projectjdk17/ds/graphs/DFSTraversal.java
+++ src/main/java/com/manoj/projectjdk17/ds/graphs/DFSTraversal.java
@@ -1,12 +1,21 @@
 package com.manoj.projectjdk17.ds.graphs;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class DFSTraversal {
 
 
     public static void main(String[] args) {
+        HashMap<Integer, List<Integer>> graph = DefineGraph.getGraph();
 
+        // source is nothing but from where we start the navigation
+        (new DFSTraversal()).depthFirstSearch(graph, 0, 7);
     }
 
+    private void depthFirstSearch(HashMap<Integer, List<Integer>> graph, int i, int i1) {
+        
+    }
 
 
 }
